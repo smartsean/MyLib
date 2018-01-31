915ec1fb0b4a882af44f7c5217d7e585583b4708
smartsean <Sean_css@163.com>
2018-01-31 18:29:41 +0800

    init

--- app/src/main/java/cn/smartsean/myutilslibrary/MainActivity.java
+++ app/src/main/java/cn/smartsean/myutilslibrary/MainActivity.java
@@ -3,7 +3,10 @@ package cn.smartsean.myutilslibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wang.avi.AVLoadingIndicatorView;
+
 import cn.smartsean.lib.utils.Logger;
+import retrofit2.Retrofit;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AVLoadingIndicatorView avLoadingIndicatorView;
+        Retrofit retrofit;
     }
 }
